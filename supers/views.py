00d0e744e3621d75b75fbd411866f43f5a5a5235
natalievmedrano <nataliemed0403@gmail.com>
2023-03-12 15:16:28 -0500

    updated serializer for POST request + updated params in views

--- supers/views.py
+++ supers/views.py
@@ -9,7 +9,13 @@ from rest_framework import status
 @api_view(['GET','POST'])
 def supers_list(request):
     if request.method == 'GET':
+        super_type = request.query_params.get('type')
+        
         supers = Super.objects.all()
+
+        if super_type: 
+           supers= supers.filter(super_type__type= super_type)
+
         serializer = SuperSerializer(supers, many= True)
         return Response(serializer.data)
 
